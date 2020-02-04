80a249e517ca895bbd27685b9ece4f75c5eda62f
Alec <alec.zaiane@gmail.com>
2020-02-04 10:12:42 -0700

    Minor changes and tests

--- src/HyperSpeed/HDeck.py
+++ src/HyperSpeed/HDeck.py
@@ -9,10 +9,10 @@ class HDeck:
 
     def connect(self):
         print("Connecting to "+self.ip)
-        print("Connection returned: "+self.Deck.connect())
+        print("Connection Attempt Finished")
         # TODO replace debug code here with something better after seeing what it actually returns
 
-    def testConnection(self):
+    def test_connection(self):
         print("Testing Connection to "+self.ip)
-        print("Ping returned: "+self.Deck.connected())
+        print("Ping returned: "+str(self.Deck.connected()))
         # TODO replace debug code with something better
--- src/HyperSpeed/HyperSpeed.py
+++ src/HyperSpeed/HyperSpeed.py
@@ -1,2 +1,6 @@
 import src.HyperDeck.HyperDeck
+import src.HyperSpeed.HDeck as HD
 
+Hd1 = HD.HDeck("10.0.1.16", "HD1")
+Hd1.connect()
+Hd1.test_connection()
