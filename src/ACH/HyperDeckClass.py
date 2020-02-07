1b9fa4f02f4b963c01b0d04b821c83d542c604be
Arthur <arthurerconggao@gmail.com>
2020-02-06 20:05:17 -0700

    Optimized command sending code

--- src/ACH/HyperDeckClass.py
+++ src/ACH/HyperDeckClass.py
@@ -1,33 +1,29 @@
 from telnetlib import Telnet
 import sys
 from multiprocessing import Process
+from time import sleep
 
-
-def send_command(tn, command):
-    print("out: "+command)
-    tn.write(bytes(command, "utf-8") + b'\r\n')
-    return tn.read_all().decode('ascii')
+tcp_port = 9993
 
 
 class HyperDeck:
 
+    def send_command(self,command):
+        print("out: " + command)
+        tn = Telnet(self.ip,tcp_port)
+        tn.write(bytes(command, "utf-8") + b'\r\n')
+        tn.write(b'quit'+b'\r\n')
+        return tn.read_all().decode('ascii')
+
     def __init__(self, ip):
         self.ip = ip
-        self.tcp_port = 9993
+        self.tn = Telnet(self.ip, tcp_port)
 
     def test_connection(self):
         print("Testing Connection to " + self.ip)
-        tn = Telnet(self.ip, self.tcp_port)
-        tn.write(b'ping' + b'\r\n')
-        tn.write(b'quit' + b'\r\n')
-        status = tn.read_all().decode('ascii')
-        print(status)
+        print(self.send_command('ping'))
 
     def send_user_command(self):
-        command = input("Please type a command to send")
-        print("Sending command to " + self.ip)
-        tn = Telnet(self.ip, self.tcp_port)
-        tn.write(bytes(command, "utf-8") + b'\r\n')
-        tn.write(b'quit' + b'\r\n')
-        status = tn.read_all().decode('ascii')
-        print(status)
+        command = input("Please type a command to send\n")
+        print("Sending '"+command+"' to " + self.ip)
+        print(self.send_command(command))
