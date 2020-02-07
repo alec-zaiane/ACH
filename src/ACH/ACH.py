c7632382a191840af441f5b39469a9f764d98a2c
Alec <alec.zaiane@gmail.com>
2020-02-07 11:02:26 -0700

    Added logging and reorganized files

--- src/ACH/ACH.py
+++ src/ACH/ACH.py
@@ -1,7 +1,27 @@
 from src.ACH.HyperDeckClass import HyperDeck
 
+
+# initialize hyperdeck list
+hyperdecks = []
+# initialize log
+main_log = []
+
+
+# define util functions
+def add_log(string):
+    main_log.append(string)
+
+
+def print_log_total():
+    for entry in main_log:
+        print(entry)
+        print("══════════════════════════")
+
+
 Hd1 = HyperDeck("10.0.1.16")
 Hd1.test_connection()
 
 while True:
     Hd1.send_user_command()
+
+
--- src/ACH/HyperDeckClass.py
+++ src/ACH/HyperDeckClass.py
@@ -1,4 +1,5 @@
 from telnetlib import Telnet
+from src.ACH.ACH import *
 import sys
 from multiprocessing import Process
 from time import sleep
@@ -8,22 +9,50 @@ tcp_port = 9993
 
 class HyperDeck:
 
-    def send_command(self, command):
-        print("out: " + command)
-        tn = Telnet(self.ip, tcp_port)
-        tn.write(bytes(command, "utf-8") + b'\r\n')
-        tn.write(b'quit'+b'\r\n')
-        return tn.read_all().decode('ascii')
-
     def __init__(self, ip):
         self.ip = ip
         self.tn = Telnet(self.ip, tcp_port)
+        self.log = []
 
-    def test_connection(self):
-        print("Testing Connection to " + self.ip)
-        print(self.send_command('ping'))
+    def add_log(self, to_log):
+        self.log.append(to_log)
+        main_log.append(self.ip+" | "+to_log)
+
+    def print_log(self):
+        for entry in self.log:
+            print(entry)
+            print("══════════════════════════")
+
+    def send_command(self, command):
+        print("send: " + command)
+        add_log("send: "+command)
+        tn = Telnet(self.ip, tcp_port)
+        tn.write(bytes(command, "utf-8") + b'\r\n')
+        # tn.write(b'quit'+b'\r\n') TODO determine if this is needed
+        self.log.append(tn.read_all().decode('ascii'))
+        main_log.append(self.log[-1])
+        return self.log[-1]
 
     def send_user_command(self):
         command = input("Please type a command to send\n")
         print("Sending '"+command+"' to " + self.ip)
         print(self.send_command(command))
+
+    def test_connection(self):
+        add_log("pinging")
+        print(self.send_command('ping'))
+
+    # speed is int, loop and single_clip are boolean
+    def play_specific(self, speed, loop, single_clip):
+        proper_speed = max(min(speed, 1600), -1600)
+        print("setting speed to "+proper_speed+" options: loop: "+loop+" single_clip: "+single_clip)
+        add_log("set speed "+proper_speed+" options: loop: "+loop+" single_clip "+single_clip)
+        self.send_command("play: speed: "+proper_speed+" loop: "+loop+" single clip: "+single_clip)
+
+    def play(self, speed):
+        proper_speed = max(min(speed, 1600), -1600)
+        print("setting speed to " + proper_speed)
+        add_log("set speed " + proper_speed)
+        self.send_command("play: speed: "+proper_speed)
+
+
